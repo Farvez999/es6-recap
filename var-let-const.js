02d433baa28fd2640aa743dd7bb0655afbaf42ba
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-26 13:56:54 +0600

    var let const declaration

---
+++ var-let-const.js
@@ -0,0 +1,26 @@
+//-------var----------//
+
+var name = 'farvez';
+name = 'Abdullah' //reassign kora jai
+var name = 'Sabbir' //redeclar kora jai
+console.log(name);
+
+//-------var----------//
+
+//-------let----------//
+
+let name = 'farvez';
+name = 'Abdullah' //reassign kora jai
+let name = 'Sabbir' //redeclar kora jai na
+console.log(name);
+
+//-------var----------//
+
+//-------let----------//
+
+const name = 'farvez';
+name = 'Abdullah' //reassign kora jai na
+let name = 'Sabbir' //redeclar kora jai na
+console.log(name);
+
+//-------var----------//
